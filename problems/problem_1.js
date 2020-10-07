2271e33e7ab361c59e790e7867f55792088ba456
Luiz Miranda <luiz_paulo6@hotmail.com>
2020-10-07 10:14:54 -0300

    Correction of some bugs
    It was necessary change the low limit 1 to 0.

--- problems/problem_1.js
+++ problems/problem_1.js
@@ -1,12 +1,12 @@
 /**
  * Find the last divisible by N, below the Target.
- * @param {number} target - Any integer bigger than 1
- * @param {number} n - Any integer bigger than 1
+ * @param {number} target - Any integer bigger than 0
+ * @param {number} n - Any integer bigger than 0
  */
 function lastDivisor(target, n) {
-  const targetIsBiggerThanN = target > n;
+  const targetIsBiggerThanN = target >= n;
   const isNumber = !(isNaN(target) && isNaN(n));
-  const biggerThanOne = target > 1 && n > 1;
+  const biggerThanOne = target > 0 && n > 0;
   const conditions = isNumber && biggerThanOne && targetIsBiggerThanN;
   return conditions ? Math.floor(target / n) : Error("Is not a correct Input");
 }
@@ -15,7 +15,7 @@ function lastDivisor(target, n) {
  * @param {number} target - Any integer bigger than 1
  */
 function sumAll(target) {
-  const condition = !isNaN(target) && target > 1;
+  const condition = !isNaN(target) && target > 0;
   return condition
     ? 0.5 * target * (target + 1)
     : Error("Is not a correct Input");
@@ -27,9 +27,21 @@ function sumAll(target) {
  */
 function sumAllDivisibleByAndBelow(target, n) {
   const divisor = lastDivisor(target, n);
+  console.log(divisor);
   return divisor && n * sumAll(divisor);
+}
+
+/**
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+function solution(target) {
+  const divisorBy3 = sumAllDivisibleByAndBelow(target, 3);
+  const divisorBy5 = sumAllDivisibleByAndBelow(target, 5);
+  const divisorBy15 = target >= 15 ? sumAllDivisibleByAndBelow(target, 15) : 0;
+  return divisorBy3 + divisorBy5 - divisorBy15;
 }
 
 exports.lastDivisor = lastDivisor;
 exports.sumAll = sumAll;
 exports.sumAllDivisibleByAndBelow = sumAllDivisibleByAndBelow;
+exports.solution = solution;
--- tests/problem_1.test.js
+++ tests/problem_1.test.js
@@ -2,6 +2,7 @@ const {
   lastDivisor,
   sumAll,
   sumAllDivisibleByAndBelow,
+  solution,
 } = require("../problems/problem_1.js");
 
 describe("problem_1", function () {
@@ -11,9 +12,9 @@ describe("problem_1", function () {
       expect(lastDivisor(27, 5)).toEqual(5);
       expect(lastDivisor(125, 7)).toEqual(17);
     });
-    it("numbers smaller than 1", function () {
+    it("numbers smaller than 0", function () {
       expect(lastDivisor(-20, -7)).toEqual(Error("Is not a correct Input"));
-      expect(lastDivisor(10, 1)).toEqual(Error("Is not a correct Input"));
+      expect(lastDivisor(10, 0)).toEqual(Error("Is not a correct Input"));
       expect(lastDivisor(15, -7)).toEqual(Error("Is not a correct Input"));
       expect(lastDivisor(-15, 3)).toEqual(Error("Is not a correct Input"));
     });
@@ -27,8 +28,7 @@ describe("problem_1", function () {
       expect(sumAll(3)).toEqual(6);
       expect(sumAll(10)).toEqual(55);
     });
-    it("number smaller or equal than 1", function () {
-      expect(sumAll(1)).toEqual(Error("Is not a correct Input"));
+    it("number smaller or equal than 0", function () {
       expect(sumAll(0)).toEqual(Error("Is not a correct Input"));
       expect(sumAll(-10)).toEqual(Error("Is not a correct Input"));
     });
@@ -49,6 +49,12 @@ describe("problem_1", function () {
     it("target is less or equal 1", function () {
       expect(sumAllDivisibleByAndBelow(1, 0)).toEqual(NaN);
       expect(sumAllDivisibleByAndBelow(-5, -15)).toEqual(NaN);
+    });
+  });
+  describe("find the sum of all the multiples of 3 or 5", function () {
+    it("known solution", function () {
+      expect(solution(10)).toEqual(23);
+      expect(solution(20)).toEqual(68);
     });
   });
 });
